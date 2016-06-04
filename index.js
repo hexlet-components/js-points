b1dd4cd124b8cf1f08ebb5fb305190380d7e991b
Kirill Mokevnin <mokevnin@gmail.com>
2016-06-04 20:07:36 +0400

    add quadrant

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ export const makePoint = (x, y) => pairs.cons(x, y);
 export const getX = (point) => pairs.car(point);
 export const getY = (point) => pairs.cdr(point);
 export const toString = (point) => pairs.toString(point);
+
+export const quadrant = (point) => {
+  const x = getX(point);
+  const y = getY(point);
+
+  if (x > 0 && y > 0) {
+    return 1;
+  } else if (x < 0 && y > 0) {
+    return 2;
+  } else if (x < 0 && y < 0) {
+    return 3;
+  } else if (x > 0 && y < 0) {
+    return 4;
+  }
+};
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import { makePoint, getX, getY, toString } from './index.js';
+import { makePoint, getX, getY, toString, quadrant } from './index.js';
 
 describe('Points', () => {
   it('#makePoint', () => {
@@ -11,5 +11,14 @@ describe('Points', () => {
   it('#toString', () => {
     const point = makePoint(10, -10);
     assert.equal(toString(point), '(10, -10)');
+  });
+
+  it('#quadrant', () => {
+    assert.equal(quadrant(makePoint(0, 0)), undefined);
+    assert.equal(quadrant(makePoint(5, 0)), undefined);
+    assert.equal(quadrant(makePoint(1, 5)), 1);
+    assert.equal(quadrant(makePoint(-3, 10)), 2);
+    assert.equal(quadrant(makePoint(-2, -5)), 3);
+    assert.equal(quadrant(makePoint(4, -1)), 4);
   });
 });
